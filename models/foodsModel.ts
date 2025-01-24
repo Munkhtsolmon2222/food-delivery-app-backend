3c8bc833f3aa83ba830d142f954ad59e720ffbfc
24LP0846 24LP0846 <24LP0846@G302-CC0089.local>
2025-01-24 20:46:13 +0800

    up

--- models/foodsModel.ts
+++ models/foodsModel.ts
@@ -1,16 +1,16 @@
 import mongoose, { Schema } from "mongoose";
 
 const FOOD_SCHEMA = new mongoose.Schema(
-  {
-    foodName: String,
-    price: Number,
-    image: String,
-    ingredients: String,
-    category: Schema.Types.ObjectId,
-  },
-  {
-    timestamps: true,
-  }
+	{
+		foodName: String,
+		price: Number,
+		image: String,
+		ingredients: String,
+		category: Schema.Types.ObjectId,
+	},
+	{
+		timestamps: true,
+	}
 );
 const foodModel = mongoose.model("Food", FOOD_SCHEMA, "food");
 export { foodModel };
